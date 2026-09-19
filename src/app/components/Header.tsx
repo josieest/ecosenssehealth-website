2ba3c1b6f164f50ab665cf82f8346da8f75c04ad
josieest <estads03@gmail.com>
2026-09-20 02:30:43 +0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu, X, ArrowUpRight } from "lucide-react";
-import logo from "src/assets/08c6febabef0a4cdbffb121033a6556c9b7694cb.png";
+import logo from "../../08c6febabef0a4cdbffb121033a6556c9b7694cb.png";
 
 export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
